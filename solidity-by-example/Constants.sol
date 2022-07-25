ee52b24fdea4f1867299ea879e5161fddfb39bbd
wstreet <wstreet7@outlook.com>
2022-07-25 23:33:12 +0800

    feat: immutable
    Change-Id: I21008c7c7cbf991de6dba2c89b0b50ce1d37212f

--- solidity-by-example/Constants.sol
+++ solidity-by-example/Constants.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+// Constants are variables that cannot be modified.
+// Their value is hard coded and using constants can save gas cost.
+
 contract Constants {
   // 大写常量变量的编码约定
   address public constant MY_ADDRESS = 0x27Ce0369ab698533eFB4a9a1a3a8E80a6De04AD4;
