05a57766a7dea5206f08ce4b9f5fd876c77905b0
wstreet <wstreet7@outlook.com>
2022-07-25 23:27:02 +0800

    feat: constant
    Change-Id: Ia81f638de1ead6278369c1300841994cc81de064

---
+++ solidity-by-example/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract Constants {
+  // 大写常量变量的编码约定
+  address public constant MY_ADDRESS = 0x27Ce0369ab698533eFB4a9a1a3a8E80a6De04AD4;
+  uint public constant MY_INT = 1;
+}
